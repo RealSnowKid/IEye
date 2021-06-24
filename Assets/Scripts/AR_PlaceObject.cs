be778f3786d807904f125e0acd58984bf48fa2d5
MartijnBassa <49745039+MartijnBassa@users.noreply.github.com>
2021-06-24 02:25:39 +0200

    Fixes

--- Assets/Scripts/AR_PlaceObject.cs
+++ Assets/Scripts/AR_PlaceObject.cs
@@ -3,91 +3,105 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 //using UnityEngine.Experimental.XR
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
 
 
-public class AR_PlaceObject : MonoBehaviour {
-    public GameObject[] objectToPlace;
+public class AR_PlaceObject : MonoBehaviour
+{
+    public GameObject objectToPlace;
     public GameObject placementIndicator;
 
     private GameObject instance = null;
-
-    private Pose PlacementPose;
-    private ARRaycastManager aRRaycastManager;
-    private bool placementPoseIsValid = false;
 
     public bool objectAlive;
     public string eyesName;
 
     public int numberOfEyes;
 
-    void Start() {
-        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
-    }
+    public bool hasGravity;
+    public bool areSelectable;
+    public bool showLabels;
+    public bool areMoveable;
 
-    void Update() {
-        UpdatePlacementPose();
-        UpdatePlacementIndicator();
+    public bool diseases = false;
 
-        if(instance != null)
-        {
-            eyesName = instance.name.Replace("(Clone)", "");
-        }
-        
-    }
-
-    private void UpdatePlacementIndicator() {
-        if (placementPoseIsValid) {
-            placementIndicator.SetActive(true);
-            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
-            placementIndicator.transform.Rotate(new Vector3(90f, 0f, 0f));
-        } else
-            placementIndicator.SetActive(false);
-    }
-
-    private void UpdatePlacementPose() {
-        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
-        var hits = new List<ARRaycastHit>();
-        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
-
-        placementPoseIsValid = hits.Count > 0;
-        if (placementPoseIsValid) {
-            PlacementPose = hits[0].pose;
-            var cameraForward = Camera.current.transform.forward;
-            var cameraBearing = new Vector3(cameraForward.x, 0f, cameraForward.z).normalized;
-            PlacementPose.rotation = Quaternion.LookRotation(cameraBearing);
-        }
-    }
-
-    public void PlaceObject() {
-        if (!placementPoseIsValid) return;
-
-        int randomObj = Random.Range(0, objectToPlace.Length);
-
+    public void PlaceObject()
+    {
         if (instance == null)
         {
-            instance = Instantiate(objectToPlace[randomObj], PlacementPose.position, PlacementPose.rotation);
+            instance = Instantiate(objectToPlace, Camera.main.transform.position, Quaternion.identity);
             instance.transform.Rotate(new Vector3(0f, 90f, 0f));
+            instance.transform.Translate(new Vector3(0f, -.25f, 0f));
+
             objectAlive = true;
             numberOfEyes++;
+
+            instance.transform.GetChild(0).GetComponent<eyeParts>().StartManual(hasGravity, areSelectable, showLabels, areMoveable);
+
+            if (diseases)
+            {
+                int rng = Random.Range(0, 2);
+
+                GameObject dis = instance.transform.GetChild(1).GetChild(rng).gameObject;
+                dis.SetActive(true);
+
+                instance.name = dis.name;
+                eyesName = instance.name;
+            }
         }
-        else {
-            instance.transform.position = PlacementPose.position;
-            instance.transform.rotation = PlacementPose.rotation;
+        else
+        {
+            instance.transform.position = Camera.main.transform.position;
+            instance.transform.Translate(new Vector3(0f, -.25f, 0f));
         }
     }
 
     public void DestroyCurrentObject()
     {
-        if(objectAlive == true)
+        if (objectAlive == true)
         {
             GameObject selected = instance.transform.GetChild(0).GetComponent<eyeParts>().selectedObject;
-            if (selected != null) {
+            if (selected != null)
+            {
                 Destroy(selected);
             }
 
             Destroy(instance);
             objectAlive = false;
-        } 
+        }
     }
 
+    public void ScaleUp()
+    {
+        AlterScale(.02f);
+    }
+
+    public void ScaleDown()
+    {
+        AlterScale(-.02f);
+    }
+
+    public void RotateLeft()
+    {
+        RotateInstance(20f);
+    }
+
+    public void RotateRight()
+    {
+        RotateInstance(-20f);
+    }
+
+    private void AlterScale(float amount)
+    {
+        if (instance == null) return;
+
+        instance.transform.localScale += new Vector3(amount, amount, amount);
+    }
+
+    private void RotateInstance(float amount)
+    {
+        if (instance == null) return;
+
+        instance.transform.Rotate(new Vector3(0f, amount, 0f));
+    }
 }
